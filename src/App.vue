a5eae3d35fbfb4af304acb6f2e409a12073e2a37
Didina Vicliuc <didinacravcenco802@gmail.com>
2023-08-04 02:28:31 +0300

    create Footer component

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,48 @@
 <template>
   <div>
-    <h1>Flower Recognition App</h1>
-    <PlantRecognition :result="result" @image-deleted="onImageDeleted" />
+    <div id="app-content">
+      <h1>Flower Recognition App</h1>
+      <PlantRecognition :result="result" @image-deleted="onImageDeleted" />
+    </div>
+    <AppFooter
+      :show="showFooter"
+      appName="Flower Recognition App"
+      creatorName="Dinuța Vicliuc"
+    />
   </div>
 </template>
 
 <script>
 import PlantRecognition from "./components/PlantRecognition.vue";
+import AppFooter from "./components/footer/AppFooter.vue";
 
 export default {
   components: {
     PlantRecognition,
+    AppFooter,
   },
   data() {
     return {
       result: null,
+      showFooter: false,
     };
   },
   methods: {
     onImageDeleted() {
       this.result = null;
     },
+    handleScroll() {
+      const scrollPosition = window.scrollY;
+      const pageHeight =
+        document.documentElement.scrollHeight - window.innerHeight;
+      this.showFooter = scrollPosition > pageHeight * 0.7;
+    },
+  },
+  mounted() {
+    window.addEventListener("scroll", this.handleScroll);
+  },
+  beforeUnmount() {
+    window.removeEventListener("scroll", this.handleScroll);
   },
 };
 </script>
---
+++ src/components/footer/AppFooter.vue
@@ -0,0 +1,26 @@
+<template>
+  <footer :class="{ footer: true, 'show-footer': show }">
+    <div class="footer-content">
+      <p>{{ appName }} - Created by {{ creatorName }}</p>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  props: {
+    appName: {
+      type: String,
+      required: true,
+    },
+    creatorName: {
+      type: String,
+      required: true,
+    },
+    show: {
+      type: Boolean,
+      required: true,
+    },
+  },
+};
+</script>
